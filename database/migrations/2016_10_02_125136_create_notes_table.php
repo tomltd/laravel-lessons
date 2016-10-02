8b10f047858597d13b0d3541a06fa39298a50534
Tom Freestone <tomfreestone@gmail.com>
2016-10-02 14:34:29 +0000

    show the notes in the card

--- database/migrations/2016_10_02_125136_create_notes_table.php
+++ database/migrations/2016_10_02_125136_create_notes_table.php
@@ -15,6 +15,8 @@ class CreateNotesTable extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('card_id')->unsigned()->index();
+            $table->text('body');
             $table->timestamps();
         });
     }
--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -4,6 +4,10 @@
 
     <h1>{{ $card->title }}</h1>
 
-
+    <ul>
+        @foreach ($card->notes as $note)
+            <li>{{ $note->body }}</li>
+        @endforeach
+    </ul>
 
 @stop
